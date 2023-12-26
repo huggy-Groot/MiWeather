aa91ddf9fd4ff13a50b4bd049240a32b96e42387
huggy-Groot <stiffgee@gmail.com>
2023-12-26 18:38:54 +0000

    Changed to Celcisus

--- miweather/src/App.js
+++ miweather/src/App.js
@@ -12,22 +12,28 @@ function App() {
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
 
-    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
-    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric)`;
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric)`;
 
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
 	const weatherResponse = await response[0].json();
 	const forcastResponse = await response[0].json();
 
-	setCurrentWeather({weatherResponse});
-	setForecast({forecastResponse});
+	setCurrentWeather({city: searchData.label, ...weatherResponse});
+	setForecast({city: searchData.label, ...forecastResponse});
+      })
+      .catch((err) => console.log(err));
   }
+  console.log(currentWeather);
+  console.log(forecast);
 
   return (
     <div className="container">
-     <Search onSearchChange={handleOnSearchChange} />
-     <CurrentWeather /> 
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 }
--- miweather/src/components/current-weather/current-weather.js
+++ miweather/src/components/current-weather/current-weather.js
@@ -1,17 +1,17 @@
 import "./current-weather.css"
 
-const CurrentWeather = () => {
+const CurrentWeather = ({data}) => {
   return (
     <div className="weather">
       <div className="top">
 	<div>
-          <p className="city">Belgrade</p>
-          <p className="weather-description">Sunny</p>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description</p>
         </div>
-        <img alt="weather" className="weather-icon" src="icons/01d.png"/>
+        <img alt="weather" className="weather-icon" src={`icons/${data.weather[0].icon}.png}`/>
       </div>
       <div className="bottom">
-	<p className="temperature">18°C</p>
+	<p className="temperature">{data.</p>
 	<div className="details">
 	  <div className="parameter-row">
 	    <span className="parameter-label">Details</span>
