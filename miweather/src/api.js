36c8fa378bc3b7c4204f289931770186e4170a88
huggy-Groot <stiffgee@gmail.com>
2023-12-26 18:09:55 +0000

    Added weather api and key and integrated it into App.js

--- miweather/src/api.js
+++ miweather/src/api.js
@@ -6,6 +6,7 @@ export const geoApiOptions = {
 	}
 };
 
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
-
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "057b52e858f2d8d689a9d7750fe28915";
