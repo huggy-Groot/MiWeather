a670e9b0ef2b976b8f96cd6c6102791268a223d9
huggy-Groot <stiffgee@gmail.com>
2023-12-26 01:57:52 +0000

    Styles

--- miweather/src/components/current-weather/current-weather.js
+++ miweather/src/components/current-weather/current-weather.js
@@ -21,7 +21,7 @@ const CurrentWeather = () => {
 	    <span className="parameter-value">44°C</span>
           </div>
 	  <div className="parameter-row">
-            <span className="parameter-label">Wind Speed</span>
+            <span className="parameter-label">Wind</span>
 	    <span className="parameter-value">5MPH</span>
 	  </div>
 	  <div className="parameter-row">
